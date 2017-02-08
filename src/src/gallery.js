82cfa133d3d675d9b9b69bdaadafe69a1b3d52b8
nprechtl <nico.prechtl@gmx.at>
2017-02-08 10:52:37 +0100

    GalleryThumberCarousel

--- src/src/gallery.js
+++ src/src/gallery.js
@@ -1,10 +1,15 @@
 /**
  * Created by nico prechtl on 08.02.2017.
  */
-
-
 $('.bxslider').bxSlider({
     auto: true,
     autoControls: true,
     pagerCustom: '#bx-pager'
+});
+
+$('.carouselPager').bxSlider({
+    minSlides: 3,
+    maxSlides: 4,
+    slideWidth: 170,
+    slideMargin: 10
 });
