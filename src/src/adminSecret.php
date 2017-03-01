4e93fd14f792a1167bbcbcff20d3802f1e75eed4
Andreas Stuhlberger <andreas.stuhlberger@web.de>
2017-03-01 11:33:03 +0100

    FTP - FileUpload (right filename needed)

--- src/src/adminSecret.php
+++ src/src/adminSecret.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" xmlns="http://www.w3.org/1999/html">
 <head>
     <meta charset="UTF-8">
     <title>SAdmin</title>
@@ -25,13 +25,29 @@ if (!isset($_SESSION['login']))
         </div>
 
         <div id="pointEdit">
+            <div>
             <Button style="margin-left: 36%" onclick="rowcolumn('hauptseitetext')">Hauptseite</Button>
             <Button>Anschrift</Button>
             <Button>E-Mail</Button>
             <Button>Telefon</Button>
             <Button>Lebenslauf</Button>
             <Button>Ausbildung</Button>
+            </div>
+            <textarea style="height: 400px;width: 600px;margin-left: 32%;margin-top: 1%;">
+
+            </textarea>
         </div>
+
+
+        <div id="pictureUpload" style="margin-left: 36%">
+            <h2>Bilder Upload</h2>
+
+            <form style="margin-top: 1%" action="upload.php" method="post" enctype="multipart/form-data">
+                <input type="file" name="file" id="passedFile">
+                <input type="submit" name="btn[upload]">
+            </form>
+        </div>
+
     <script>
         function rowcolumn($table) {
             alert("hallo");
